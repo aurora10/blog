7d105d1b8abdcc23c88fcd16a7a372a670dfab79
aurora10 <aurora10@gmail.com>
2019-11-01 14:56:40 +0100

    works til page 32

---
+++ admin/includes/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function classAutoloader($class) {
+    $class = strtolower($class);
+    $the_path = "includes/{$class}.php";
+
+    if (is_file($the_path))
+        if (file_exists($the_path) && !class_exists($class)) {
+            include ($the_path);
+        }else{
+            die("The file name {$class}.php was not found!");
+        }
+
+}
+
+
+spl_autoload_register('classAutoLoader');
+
+function redirect($location) {
+    header("Location: {$location}");
+}
---
+++ admin/includes/init.php
@@ -0,0 +1,7 @@
+<?php require_once("functions.php"); ?>
+
+<?php require_once("Database.php"); ?>
+<?php require_once("config.php"); ?>
+<?php require_once("User.php"); ?>
+<?php require_once("Session.php"); ?>
+
